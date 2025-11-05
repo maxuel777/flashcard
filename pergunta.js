3697d5a881bf12a06f5ca54c2461689f65c3959c
maxuel777 <maxuel.leporaci@escola.pr.gov.br>
2025-11-05 00:34:33 +0000

    final

---
+++ pergunta.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'matematica',
+    'João tem 12 maçãs e dá 4 para sua amiga. Quantas maçãs João tem agora?',
+    'João começa com 12 maçãs e dá 4 para sua amiga. Para saber quantas maçãs ele tem agora, subtraímos 4 de 12 que fica igual a 8'
+)
+
+criaCartao(
+    'portugues',
+    'Qual é o plural de "cão"?',
+    'O plural de "cão" é "cães"'
+)
+
+criaCartao(
+    'historia',
+    'Quem foi o primeiro presidente do Brasil??',
+    'O primeiro presidente do Brasil foi Deodoro da Fonseca'
+)
+
+criaCartao(
+    'ingles',
+    'What is your favorite color?"?',
+    'My favorite color is blue."'
+)
+
+criaCartao(
+    'quimica',
+    'Qual é o símbolo do elemento oxigênio?"?',
+    'O símbolo do elemento oxigênio é O."'
+)
+
+criaCartao(
+    'biologia',
+    'Qual é a principal função do coração?"?',
+    'A principal função do coração é bombear o sangue por todo o corpo, levando oxigênio e nutrientes às células."'
+)
+
+criaCartao(
+    'sociologia',
+    'O que estuda a Sociologia?"?',
+    'A Sociologia estuda a sociedade, as relações entre as pessoas e o comportamento humano em grupo."'
+)
+
+criaCartao(
+    'educacao fisica',
+    'Qual é o principal benefício da prática regular de exercícios físicos?"?',
+    'O principal benefício é melhorar a saúde do corpo e da mente, ajudando na disposição, na força e na prevenção de doenças."'
+)
+
+criaCartao(
+    'projeto de vida',
+    'O que é um projeto de vida?"?',
+    'Um projeto de vida é um plano que ajuda a pessoa a definir seus objetivos, sonhos e ações para o futuro, guiando suas escolhas e decisões."'
+)
+
+criaCartao(
+    'fisica',
+    'O que é a força da gravidade?"?',
+    'A força da gravidade é a atração que a Terra exerce sobre todos os corpos, fazendo com que tudo seja puxado para o seu centro."'
+)
